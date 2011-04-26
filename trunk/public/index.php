230ed629a6af2f7777e8a748f2e03409fc432858
correodavid@gmail.com <correodavid@gmail.com@41ad143f-68f6-4b0d-8475-91f4d24ca4af>
2011-04-26 13:19:21 +0000

    añadido el framework para css blueprint creada la constante APPLICATION_PATH

--- trunk/public/index.php
+++ trunk/public/index.php
@@ -1,5 +1,11 @@
 <?php
-phpinfo();
+// Define ruta al directorio application
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
+// Añade el fichero de arraque de la aplicacion
+require_once APPLICATION_PATH."/Bootstrap.php";
 
+// Carga el layout
+include_once APPLICATION_PATH."/layouts/layout.php";
 
