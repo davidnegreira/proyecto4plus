4cfd6c8e2ee90ec16825d4c149b2681429ec31df
correodavid <correodavid@gmail.com>
2011-04-26 13:19:21 +0000

    añadido el framework para css blueprint creada la constante APPLICATION_PATH

---
+++ application/layouts/layout.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Título</title>
+	
+    <!-- Framework CSS -->
+    <link rel="stylesheet" href="/styles/blueprint/screen.css" type="text/css" media="screen, projection">
+    <link rel="stylesheet" href="/styles/blueprint/print.css" type="text/css" media="print">
+    <!--[if lt IE 8]><link rel="stylesheet" href="/styles/blueprint/ie.css" type="text/css" media="screen, projection"><![endif]-->
+
+</head>
+
+<body>
+
+<!-- Las columnas miden 30px. Se pueden agrupar. El tamaño resultantes lo da la siguente formula: anchura = (columnas_que_unimos * 40) - 10 -->
+
+<!-- Contenedor Principal de la Pagina -->
+<div class="container">
+	<div class="span-24 last" style="background-color:blue">
+			<h1>Prueba del layout Blueprint</h1>
+	</div>
+
+	<div class="span-18" >
+		<p>Cuerpo izquierda...</p>
+		
+		<p>		
+		Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. Quo mundi lobortis reformidans eu, legimus senserit definiebas an eos. Eu sit tincidunt incorrupte definitionem, vis mutat affert percipit cu, eirmod consectetuer signiferumque eu per. In usu latine equidem dolores. Quo no falli viris intellegam, ut fugit veritus placerat per.
+		Ius id vidit volumus mandamus, vide veritus democritum te nec, ei eos debet libris consulatu. No mei ferri graeco dicunt, ad cum veri accommodare. Sed at malis omnesque delicata, usu et iusto zzril meliore. Dicunt maiorum eloquentiam cum cu, sit summo dolor essent te. Ne quodsi nusquam legendos has, ea dicit voluptua eloquentiam pro, ad sit quas qualisque. Eos vocibus deserunt quaestio ei.
+		Blandit incorrupte quaerendum in quo, nibh impedit id vis, vel no nullam semper audiam. Ei populo graeci consulatu mei, has ea stet modus phaedrum. Inani oblique ne has, duo et veritus detraxit. Tota ludus oratio ea mel, offendit persequeris ei vim. Eos dicat oratio partem ut, id cum ignota senserit intellegat. Sit inani ubique graecis ad, quando graecis liberavisse et cum, dicit option eruditi at duo. Homero salutatus suscipiantur eum id, tamquam voluptaria expetendis ad sed, nobis feugiat similique usu ex.
+		Eum hinc argumentum te, no sit percipit adversarium, ne qui feugiat persecuti. Odio omnes scripserit ad est, ut vidit lorem maiestatis his, putent mandamus gloriatur ne pro. Oratio iriure rationibus ne his, ad est corrumpit splendide. Ad duo appareat moderatius, ei falli tollit denique eos. Dicant evertitur mei in, ne his deserunt perpetua sententiae, ea sea omnes similique vituperatoribus. Ex mel errem intellegebat comprehensam, vel ad tantas antiopam delicatissimi, tota ferri affert eu nec. Legere expetenda pertinacia ne pro, et pro impetus persius assueverit.
+		</p>		
+	
+		<div class="span-5 colborder">
+			<h2>Columna 1</h2>
+			<p>Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. </p>
+		</div>
+		
+		<div class="span-5 colborder">
+			<h2>Columna 2</h2>
+			<p>Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. </p>			
+		</div>
+		
+		<div class="span-5 last">
+			<h2>Columna 3</h2>
+			<p>Lorem ipsum ad his scripta blandit partiendo, eum fastidii accumsan euripidis in, eum liber hendrerit an. Qui ut wisi vocibus suscipiantur, quo dicit ridens inciderint id. </p>
+		</div>
+	</div>
+
+	<div class="span-6 last" style="background-color:red;">
+		<div ">
+			<h3>Lateral</h3>
+			<ul>
+			<li><a>enlace1</a></li>
+			<li><a>enlace2</a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<div class="span-24 last" style="background-color:blue;">
+		<h2>Pie</h2>
+	</div>	
+</div> 
+
+</body>
+</html>
