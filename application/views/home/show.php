e56b7c43ce0c4f0b2a33d1ed9fbb2aed12dcd2cf
correodavid <correodavid@gmail.com>
2011-05-11 11:27:24 +0000

    Añadida columna derecha Donde estamos

--- application/views/home/show.php
+++ application/views/home/show.php
@@ -1,13 +1,14 @@
-<div class="span-5 border" id="menu">
+<div class="span-6" id="menu">
 	<div class="contenido-izq">
 	<?php if (isset($page["contenido-izq"]) ){
 		echo $page["contenido-izq"]["title"];
 		echo $page["contenido-izq"]["value"];
+		
 	}?>	
 	</div>
 </div>
 
-<div class="span-19 last">
+<div class="span-18 last">
 	<p>
 	<?php if (isset($page["principal"]) ){
 		echo $page["principal"]["title"];
@@ -15,24 +16,34 @@
 	}?>
 	</p>			
 
-	<div class="span-6 colborder">
-		<?php if (isset($page["col1"]) ){
-			echo $page["col1"]["title"];
-			echo $page["col1"]["value"];
-		}?>		
+	
+
+	<div class="span-6">
+		<div class="borde-sombra">
+			<?php if (isset($page["col1"]) ){
+				echo $page["col1"]["title"];
+				echo $page["col1"]["value"];
+			}?>
+		</div>			
 	</div>
 	
-	<div class="span-6 colborder">
+	<div class="span-6">
+		<div class="borde-sombra">
 		<?php if (isset($page["col2"]) ){
 			echo $page["col2"]["title"];
 			echo $page["col2"]["value"];
-		}?>		
+		}?>
+		</div>		
 	</div>
 	
-	<div class="span-5 last">
+	<div class="span-6 borde-sombra-der last">
+		<div class="borde-sombra" style="padding-right:10px">
 		<?php if (isset($page["col3"]) ){
 			echo $page["col3"]["title"];
 			echo $page["col3"]["value"];
-		}?>	
+		}?>
+		</div>
 	</div>
+	
+	
 </div>
